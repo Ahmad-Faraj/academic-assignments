1d04b208478f2057d0850f63df3dd6c9ce085718
Ahmed Faraj <148560615+Ahmad-Faraj@users.noreply.github.com>
2025-06-17 16:29:00 +0300

    Update Factor_GCD.cpp

--- sem4/algorithms/final/Math/Factor_GCD.cpp
+++ sem4/algorithms/final/Math/Factor_GCD.cpp
@@ -1,17 +1,20 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 
-// Computes GCD using prime factorization
+// Computes GCD using prime factorization up to sqrt(min(m,n))
 // Time Complexity: O(sqrt(min(m,n)))
 // Space Complexity: O(1)
 int Factor_GCD(int m, int n) {
     int gcd = 1;
-    for (int i = 2; i <= min(m, n); i++) {
+    int limit = sqrt(min(m, n));
+    for (int i = 2; i <= limit; i++) {
         while (m % i == 0 && n % i == 0) {
             gcd *= i;
             m /= i;
             n /= i;
         }
     }
+    if (m == n) gcd *= m;
     return gcd;
 }
